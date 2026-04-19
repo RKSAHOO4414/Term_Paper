cccc2689cfd8ee1b8092b1c34fe7a4342c350c97
Ranjan Kumar Sahoo <97281672+RKSAHOO4414@users.noreply.github.com>
2026-04-19 10:44:44 +0530

    update cmake

---
+++ build/CMakeFiles/batch_sim.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/batch_sim.dir/link.d"
+  "CMakeFiles/batch_sim.dir/src/main.cpp.o"
+  "CMakeFiles/batch_sim.dir/src/main.cpp.o.d"
+  "bin/batch_sim"
+  "bin/batch_sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/batch_sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
